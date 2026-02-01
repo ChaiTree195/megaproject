99c775bbc9f18edf8fb3241338435560670fab59
EDGARING <zh.edgar08@mail.ru>
2026-02-02 00:38:19 +0300

    АААААААААААААААААААААААААА

--- Tileset_2D Platformer.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Tileset_2D Platformer" tilewidth="16" tileheight="16" tilecount="315" columns="21">
- <image source="../../Downloads/Tileset_Templates/Tileset_Templates/Tileset_2D Platformer.png" width="336" height="240"/>
-</tileset>
